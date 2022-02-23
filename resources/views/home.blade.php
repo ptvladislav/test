aceeef211c7c9ea86bb2ff571451cb12ba994995
ptvladislav <77277799+ptvladislav@users.noreply.github.com>
2022-02-23 16:12:26 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,11 @@
                             {{ session('userNotFound') }}
                         </div>
                     @endif
+                       @if (session('transferError'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ session('transferError') }}
+                        </div>
+                    @endif
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
